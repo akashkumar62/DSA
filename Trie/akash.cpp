1de3d11eb9cf73025f076aaabe5b4c0bc426fdd4
akashkumar62 <xiome555@gmail.com>
2024-03-27 23:43:58 +0530

    problem added

--- Trie/akash.cpp
+++ Trie/akash.cpp
@@ -1,3 +1,16 @@
+// You are given an array ‘ARR’ consisting of ‘N’ strings. Your task is to find the longest
+// common prefix among all these strings. 
+//If there is no common prefix, you have to return an empty string.
+
+// A prefix of a string can be defined as a substring obtained after removing some or all 
+//characters from the end of the string.
+
+// For Example:
+// Consider ARR = [“coding”, ”codezen”, ”codingninja”, ”coders”]
+// The longest common prefix among all the given strings is “cod” as it is present as a prefix in all strings. Hence, the answer is “cod”.
+
+
+
 
 #include <bitset>/stdc++.h>
 using namespace std;
