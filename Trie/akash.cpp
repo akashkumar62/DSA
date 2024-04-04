da312a22c23aa917b1368574d2c34579324dbe0c
akashkumar62 <xiome555@gmail.com>
2024-04-04 12:04:34 +0530

    trie implementation

--- Trie/akash.cpp
+++ Trie/akash.cpp
@@ -12,11 +12,84 @@
 
 
 
-#include <bits/stdc++.h>
-using namespace std;
 
-int main(){
-    cout << "Hello Worldvhgvvvjhvjh" << endl;
+struct Node {
+    Node* Link[26];
+    bool flag;
 
-    return 0;
+    Node() {
+        for (int i = 0; i < 26; ++i)
+            Link[i] = nullptr;
+        flag = false; // Should only be true for the end of a word
+    }
+
+    void put(char ch, Node* node) {
+        Link[ch - 'a'] = node;
+    }
+
+    Node* get(char ch) {
+        return Link[ch - 'a'];
+    }
+
+    void setEnd() {
+        flag = true;
+    }
+
+    bool isEnd() {
+        return flag;
+    }
+
+    bool containsKey(char ch) {
+        return Link[ch - 'a'] != nullptr;
+    }
+};
+
+class Trie {
+private:
+    Node* root;
+
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(const string& word) {
+        Node* node = root;
+        for (char ch : word) {
+            if (!node->containsKey(ch))
+                node->put(ch, new Node());
+            node = node->get(ch);
+        }
+        node->setEnd();
+    }
+
+    string longestPrefix() {
+        Node* node = root;
+        string ans;
+        while (node) {
+            int cnt = 0;
+            char ch;
+            for (int i = 0; i < 26; i++) {
+                if (node->containsKey(char(i + 'a'))) {
+                    cnt++;
+                    ch = i + 'a';
+                }
+            }
+            if (cnt != 1 || node->isEnd())
+                break;
+            ans += ch;
+            node = node->get(ch);
+        }
+        return ans;
+    }
+};
+
+string longestCommonPrefix(vector<string>& arr, int n) {
+    Trie tree;
+
+    for (auto x : arr) {
+        tree.insert(x);
+    }
+
+    return tree.longestPrefix();
 }
